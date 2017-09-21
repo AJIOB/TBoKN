636c2fc59d6f3aa0165f22445b8030ac9deb877c
AJIOB <aleksandr9809@gmail.com>
2017-09-21 21:45:01 +0300

    Internal logger was added

---
+++ TOKS_lab1/backend/InternalLogger.cs
@@ -0,0 +1,12 @@
+﻿using log4net;
+
+namespace TOKS_lab1.backend
+{
+    /// <summary>
+    /// log4net wrapper
+    /// </summary>
+    public static class InternalLogger
+    {
+        public static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+    }
+}
