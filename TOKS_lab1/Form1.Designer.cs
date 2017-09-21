e73c527941fd5005181c5a95830c43300cfc7af9
AJIOB <aleksandr9809@gmail.com>
2017-09-21 21:24:29 +0300

    Form1: my ID and partner ID fields were added

--- TOKS_lab1/Form1.Designer.cs
+++ TOKS_lab1/Form1.Designer.cs
@@ -46,9 +46,15 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.inputTextBox = new System.Windows.Forms.TextBox();
             this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.myIdNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.partnerIdNumeric = new System.Windows.Forms.NumericUpDown();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel4.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.myIdNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.partnerIdNumeric)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -67,7 +73,7 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.7384F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(466, 243);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(466, 293);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
             // tableLayoutPanel4
@@ -79,7 +85,6 @@
             this.tableLayoutPanel4.ColumnCount = 2;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55F));
-            this.tableLayoutPanel4.Controls.Add(this.startStopButton, 0, 6);
             this.tableLayoutPanel4.Controls.Add(this.currentPortLabel, 0, 0);
             this.tableLayoutPanel4.Controls.Add(this.baudrateLabel, 0, 1);
             this.tableLayoutPanel4.Controls.Add(this.dataBitsLabel, 0, 2);
@@ -92,17 +97,24 @@
             this.tableLayoutPanel4.Controls.Add(this.parityComboBox, 1, 4);
             this.tableLayoutPanel4.Controls.Add(this.flowControlComboBox, 1, 5);
             this.tableLayoutPanel4.Controls.Add(this.currentPortComboBox, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.startStopButton, 0, 8);
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 6);
+            this.tableLayoutPanel4.Controls.Add(this.label2, 0, 7);
+            this.tableLayoutPanel4.Controls.Add(this.myIdNumeric, 1, 6);
+            this.tableLayoutPanel4.Controls.Add(this.partnerIdNumeric, 1, 7);
             this.tableLayoutPanel4.Location = new System.Drawing.Point(236, 3);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
-            this.tableLayoutPanel4.RowCount = 7;
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(227, 237);
+            this.tableLayoutPanel4.RowCount = 9;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(227, 287);
             this.tableLayoutPanel4.TabIndex = 5;
             // 
             // startStopButton
@@ -111,7 +123,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel4.SetColumnSpan(this.startStopButton, 2);
-            this.startStopButton.Location = new System.Drawing.Point(3, 201);
+            this.startStopButton.Location = new System.Drawing.Point(3, 251);
             this.startStopButton.Name = "startStopButton";
             this.startStopButton.Size = new System.Drawing.Size(221, 33);
             this.startStopButton.TabIndex = 0;
@@ -127,7 +139,7 @@
             this.currentPortLabel.AutoSize = true;
             this.currentPortLabel.Location = new System.Drawing.Point(3, 0);
             this.currentPortLabel.Name = "currentPortLabel";
-            this.currentPortLabel.Size = new System.Drawing.Size(96, 33);
+            this.currentPortLabel.Size = new System.Drawing.Size(96, 31);
             this.currentPortLabel.TabIndex = 0;
             this.currentPortLabel.Text = "Current port";
             this.currentPortLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -138,9 +150,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.baudrateLabel.AutoSize = true;
-            this.baudrateLabel.Location = new System.Drawing.Point(3, 33);
+            this.baudrateLabel.Location = new System.Drawing.Point(3, 31);
             this.baudrateLabel.Name = "baudrateLabel";
-            this.baudrateLabel.Size = new System.Drawing.Size(96, 33);
+            this.baudrateLabel.Size = new System.Drawing.Size(96, 31);
             this.baudrateLabel.TabIndex = 0;
             this.baudrateLabel.Text = "Baudrate";
             this.baudrateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -151,9 +163,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataBitsLabel.AutoSize = true;
-            this.dataBitsLabel.Location = new System.Drawing.Point(3, 66);
+            this.dataBitsLabel.Location = new System.Drawing.Point(3, 62);
             this.dataBitsLabel.Name = "dataBitsLabel";
-            this.dataBitsLabel.Size = new System.Drawing.Size(96, 33);
+            this.dataBitsLabel.Size = new System.Drawing.Size(96, 31);
             this.dataBitsLabel.TabIndex = 0;
             this.dataBitsLabel.Text = "Data bits";
             this.dataBitsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -164,9 +176,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.stopBitsLabel.AutoSize = true;
-            this.stopBitsLabel.Location = new System.Drawing.Point(3, 99);
+            this.stopBitsLabel.Location = new System.Drawing.Point(3, 93);
             this.stopBitsLabel.Name = "stopBitsLabel";
-            this.stopBitsLabel.Size = new System.Drawing.Size(96, 33);
+            this.stopBitsLabel.Size = new System.Drawing.Size(96, 31);
             this.stopBitsLabel.TabIndex = 0;
             this.stopBitsLabel.Text = "Stop bits";
             this.stopBitsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -177,9 +189,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.parityLabel.AutoSize = true;
-            this.parityLabel.Location = new System.Drawing.Point(3, 132);
+            this.parityLabel.Location = new System.Drawing.Point(3, 124);
             this.parityLabel.Name = "parityLabel";
-            this.parityLabel.Size = new System.Drawing.Size(96, 33);
+            this.parityLabel.Size = new System.Drawing.Size(96, 31);
             this.parityLabel.TabIndex = 0;
             this.parityLabel.Text = "Parity";
             this.parityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -190,9 +202,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowControlLabel.AutoSize = true;
-            this.flowControlLabel.Location = new System.Drawing.Point(3, 165);
+            this.flowControlLabel.Location = new System.Drawing.Point(3, 155);
             this.flowControlLabel.Name = "flowControlLabel";
-            this.flowControlLabel.Size = new System.Drawing.Size(96, 33);
+            this.flowControlLabel.Size = new System.Drawing.Size(96, 31);
             this.flowControlLabel.TabIndex = 0;
             this.flowControlLabel.Text = "Flow control";
             this.flowControlLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -204,7 +216,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.baudrateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.baudrateComboBox.FormattingEnabled = true;
-            this.baudrateComboBox.Location = new System.Drawing.Point(105, 36);
+            this.baudrateComboBox.Location = new System.Drawing.Point(105, 34);
             this.baudrateComboBox.Name = "baudrateComboBox";
             this.baudrateComboBox.Size = new System.Drawing.Size(119, 21);
             this.baudrateComboBox.TabIndex = 1;
@@ -216,7 +228,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataBitsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.dataBitsComboBox.FormattingEnabled = true;
-            this.dataBitsComboBox.Location = new System.Drawing.Point(105, 69);
+            this.dataBitsComboBox.Location = new System.Drawing.Point(105, 65);
             this.dataBitsComboBox.Name = "dataBitsComboBox";
             this.dataBitsComboBox.Size = new System.Drawing.Size(119, 21);
             this.dataBitsComboBox.TabIndex = 1;
@@ -228,7 +240,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.stopBitsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.stopBitsComboBox.FormattingEnabled = true;
-            this.stopBitsComboBox.Location = new System.Drawing.Point(105, 102);
+            this.stopBitsComboBox.Location = new System.Drawing.Point(105, 96);
             this.stopBitsComboBox.Name = "stopBitsComboBox";
             this.stopBitsComboBox.Size = new System.Drawing.Size(119, 21);
             this.stopBitsComboBox.TabIndex = 1;
@@ -240,7 +252,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.parityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.parityComboBox.FormattingEnabled = true;
-            this.parityComboBox.Location = new System.Drawing.Point(105, 135);
+            this.parityComboBox.Location = new System.Drawing.Point(105, 127);
             this.parityComboBox.Name = "parityComboBox";
             this.parityComboBox.Size = new System.Drawing.Size(119, 21);
             this.parityComboBox.TabIndex = 1;
@@ -252,7 +264,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowControlComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.flowControlComboBox.FormattingEnabled = true;
-            this.flowControlComboBox.Location = new System.Drawing.Point(105, 168);
+            this.flowControlComboBox.Location = new System.Drawing.Point(105, 158);
             this.flowControlComboBox.Name = "flowControlComboBox";
             this.flowControlComboBox.Size = new System.Drawing.Size(119, 21);
             this.flowControlComboBox.TabIndex = 1;
@@ -284,7 +296,7 @@
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.7384F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.2616F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(227, 237);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(227, 287);
             this.tableLayoutPanel2.TabIndex = 3;
             // 
             // inputTextBox
@@ -296,7 +308,7 @@
             this.inputTextBox.Location = new System.Drawing.Point(3, 3);
             this.inputTextBox.Multiline = true;
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(221, 55);
+            this.inputTextBox.Size = new System.Drawing.Size(221, 67);
             this.inputTextBox.TabIndex = 1;
             this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
             // 
@@ -305,20 +317,76 @@
             this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.outputTextBox.Location = new System.Drawing.Point(3, 64);
+            this.outputTextBox.Location = new System.Drawing.Point(3, 76);
             this.outputTextBox.Multiline = true;
             this.outputTextBox.Name = "outputTextBox";
             this.outputTextBox.ReadOnly = true;
             this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.outputTextBox.Size = new System.Drawing.Size(221, 170);
+            this.outputTextBox.Size = new System.Drawing.Size(221, 208);
             this.outputTextBox.TabIndex = 1;
             this.outputTextBox.WordWrap = false;
+            // 
+            // myIdNumeric
+            // 
+            this.myIdNumeric.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.myIdNumeric.Location = new System.Drawing.Point(105, 189);
+            this.myIdNumeric.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.myIdNumeric.Name = "myIdNumeric";
+            this.myIdNumeric.Size = new System.Drawing.Size(119, 20);
+            this.myIdNumeric.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My ID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Partner ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // partnerIdNumeric
+            // 
+            this.partnerIdNumeric.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.partnerIdNumeric.Location = new System.Drawing.Point(105, 220);
+            this.partnerIdNumeric.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.partnerIdNumeric.Name = "partnerIdNumeric";
+            this.partnerIdNumeric.Size = new System.Drawing.Size(119, 20);
+            this.partnerIdNumeric.TabIndex = 2;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.ClientSize = new System.Drawing.Size(484, 311);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MainWindow";
             this.Text = "Serial port communicator by AJIOB";
@@ -327,6 +395,8 @@
             this.tableLayoutPanel4.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.myIdNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.partnerIdNumeric)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -350,6 +420,10 @@
         private System.Windows.Forms.ComboBox stopBitsComboBox;
         private System.Windows.Forms.ComboBox parityComboBox;
         private System.Windows.Forms.ComboBox flowControlComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown myIdNumeric;
+        private System.Windows.Forms.NumericUpDown partnerIdNumeric;
     }
 }
 
