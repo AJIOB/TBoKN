483e9f80f033073d7f4eccc10108e4c64f19f25d
AJIOB <aleksandr9809@gmail.com>
2017-10-09 20:41:30 +0300

    BytesToBits converter was written

--- lab3/HammingCodeProgram/HammingCodeProgram/MyLib.cpp
+++ lab3/HammingCodeProgram/HammingCodeProgram/MyLib.cpp
@@ -1,3 +1,5 @@
+#include <bitset>
+
 #include "MyLib.h"
 
 const int messageLengthInBytes = 2;
@@ -12,7 +14,7 @@ std::deque<bool> EncodeMessage(std::string message)
 		throw std::exception("First argument must contains 2 symbols only");
 	}
 
-	std::deque<bool> result;
+	std::deque<bool> result = BytesToBits(message);
 
 	//TODO
 
@@ -23,4 +25,28 @@ std::string DecodeMessage(std::deque<bool> message, int& numOfFoundErrors)
 {
 	//TODO
 	return std::string();
+}
+
+std::deque<bool> BytesToBits(std::string bytes)
+{
+	std::deque<bool> result;
+	for (auto c : bytes)
+	{
+		std::bitset<bitsInByte> bitset(c);
+		for (auto i = 0U; i < bitset.size(); ++i)
+		{
+			result.push_back(bitset[i]);
+		}
+	}
+
+	return result;
+}
+
+std::string BitsToBytes(std::deque<bool> bits)
+{
+	std::string result;
+
+	//TODO
+
+	return result;
 }
--- lab3/HammingCodeProgram/HammingCodeProgram/MyLib.h
+++ lab3/HammingCodeProgram/HammingCodeProgram/MyLib.h
@@ -3,3 +3,6 @@
 
 std::deque<bool> EncodeMessage(std::string message);
 std::string DecodeMessage(std::deque<bool> message, int& numOfFoundErrors);
+
+std::deque<bool> BytesToBits(std::string bytes);
+std::string BitsToBytes(std::deque<bool> bits);
