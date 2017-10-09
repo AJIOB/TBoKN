7ac35c6946a466c70665d0961fbafc46c467e65a
AJIOB <aleksandr9809@gmail.com>
2017-10-09 13:31:26 +0300

    CMD arguments checking was added

--- lab3/HammingCodeProgram/HammingCodeProgram/main.cpp
+++ lab3/HammingCodeProgram/HammingCodeProgram/main.cpp
@@ -3,11 +3,26 @@
 
 int main(int argc, char* argv[])
 {
+	const int messageLengthInBytes = 2;
+	const int bitsInByte = 8;
+	const int numOfControlBits = 5;
+
 	int errorCode = 0;
 
 	try
 	{
 		std::cout << "Hamming code example application was started" << std::endl;
+
+		if (argc != 2)
+		{
+			throw std::exception("Need to pass only one argument: message to encode");
+		}
+
+		std::string inputMessage(argv[1]);
+		if (inputMessage.length() != messageLengthInBytes)
+		{
+			throw std::exception("First argument must contains 2 symbols only");
+		}
 
 		//TODO: write code
 
