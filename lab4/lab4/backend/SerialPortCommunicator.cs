dd282f1406ded2b380700a59984f2e44869e70e2
AJIOB <aleksandr9809@gmail.com>
2017-10-22 16:51:33 +0300

    SerialPortCommunicator: simple sender was made

--- lab4/lab4/backend/SerialPortCommunicator.cs
+++ lab4/lab4/backend/SerialPortCommunicator.cs
@@ -21,31 +21,13 @@ namespace lab4.backend
         }
 
         private const int BitsInByte = 8;
-        private const byte StartStopByte = 0x7E;
-
-        private const byte BitStaffingCheckMask = 0x7E;
-        private const byte BitStaffingAndMask = 0xFE;
-
-        // not 0x7F, because start parsing (receiving) data from low-order bit
-        private const byte BitStaffingReplaceSymbol = 0xFE;
-
-        private const byte GetLastBitMask = 0x01;
-        private readonly List<bool> _receivedBuffer = new List<bool>();
-
-        private byte _myId = 0;
-        private byte _partnerId = 0;
-
-        public byte MyId
-        {
-            get => _myId;
-            set => ChangeProperty(ref value, ref _myId, nameof(MyId));
-        }
-
-        public byte PartnerId
-        {
-            get => _partnerId;
-            set => ChangeProperty(ref value, ref _partnerId, nameof(PartnerId));
-        }
+        private const byte JamByte = 0xFF;
+        private const byte EscapeByte = 0x01;
+        private const byte JamByteSecondInEscape = 0x00;
+        private const byte EscapeByteSecondInEscape = 0x02;
+        private const int TimeToWaitNextByteInMs = 100;
+        
+        private readonly List<byte> _receivedBuffer = new List<byte>();
 
         public delegate void ReceivedEventHandler(object sender, EventArgs e);
 
@@ -117,10 +99,13 @@ namespace lab4.backend
         public void Send(string s)
         {
             InternalLogger.Log.Debug($"Sending string: \"{s}\"");
-            var dataToSend = GeneratePacket(Encoding.UTF8.GetBytes(s).ToArray()).ToArray();
-            SerialPort.Write(dataToSend, 0, dataToSend.Length);
+            var dataToSend = GeneratePacket(Encoding.ASCII.GetBytes(s)).ToArray();
+            foreach (var b in dataToSend)
+            {
+                SerialPort.Write(new []{b}, 0, 1);
+            }
         }
-
+        
         /// <summary>
         /// Closing current connection
         /// </summary>
@@ -167,27 +152,13 @@ namespace lab4.backend
                     InternalLogger.Log.Info("End of the stream was read");
                     break;
                 }
-                _receivedBuffer.AddRange(BytesToBools(new[] {(byte) received}));
+                _receivedBuffer.Add((byte) received);
             }
 
-            IEnumerable<byte> data = null;
-            try
-            {
-                int index;
-                data = ParsePacket(_receivedBuffer, out index);
-                _receivedBuffer.RemoveRange(0, index);
-            }
-            catch (CannotFindStopSymbolException)
-            {
-                //All is good
-                return "";
-            }
-            catch (CannotFindStartSymbolException)
-            {
-                _receivedBuffer.Clear();
-                throw;
-            }
-            return data != null ? Encoding.UTF8.GetString(data.ToArray()) : "";
+            var parsed = ParsePacket(_receivedBuffer, out var index);
+            _receivedBuffer.RemoveRange(0, index);
+            
+            return Encoding.ASCII.GetString(parsed.ToArray());
         }
 
         /// <summary>
@@ -195,97 +166,9 @@ namespace lab4.backend
         /// </summary>
         /// <param name="inputBytes">Bytes to convert with bit staffing</param>
         /// <returns>Converted input value</returns>
-        private IEnumerable<bool> Encode(IEnumerable<byte> inputBytes)
+        private IEnumerable<byte> Encode(IEnumerable<byte> inputBytes)
         {
-            var res = BytesToBools(inputBytes).ToList();
-
-            for (var i = 0; i < (res.Count - BitsInByte + 1); ++i)
-            {
-                var b = BoolsToBytes(res.GetRange(i, BitsInByte)).First();
-                var isFindByteToStuffing = (((b & BitStaffingAndMask) ^ BitStaffingCheckMask) == 0);
-
-                if (!isFindByteToStuffing) continue;
-
-                //Skipping staffed 8 bits
-                i += BitsInByte - 1;
-                res.Insert(i, true);
-            }
-
-            return res;
-        }
-
-        /// <summary>
-        /// Decode bit array by deletinig bit staffing
-        /// </summary>
-        /// <param name="inputBits">Bits to decode with bit staffing</param>
-        /// <param name="index">Index of first non-used element (number of used bits)</param>
-        /// <returns>Decoded input value</returns>
-        private IEnumerable<bool> Decode(IEnumerable<bool> inputBits, out int index)
-        {
-            var res = inputBits.ToList();
-
-            for (index = 0; index < (res.Count - BitsInByte + 1); ++index)
-            {
-                var b = BoolsToBytes(res.GetRange(index, BitsInByte)).First();
-
-                if (b == StartStopByte)
-                {
-                    //removing all to the end from stop symbol
-                    res.RemoveRange(index, res.Count - index);
-                    index += BitsInByte;
-                    return res;
-                }
-                if (b == BitStaffingReplaceSymbol)
-                {
-                    //Skipping staffed 8 bits (will be 7 after deleting bit)
-                    index += BitsInByte - 2;
-                    res.RemoveAt(index + 1);
-                }
-            }
-
-            throw new CannotFindStopSymbolException();
-        }
-
-        /// <summary>
-        /// Check is packet to me
-        /// </summary>
-        /// <param name="packet">Packet to check</param>
-        /// <returns>True if packet addressed to me, else false</returns>
-        private bool IsPacketToMe(IEnumerable<byte> packet)
-        {
-            return (packet.First() == MyId);
-        }
-
-        /// <summary>
-        /// Delete address metadata from data
-        /// </summary>
-        /// <param name="data">Data to delete address metadata</param>
-        /// <returns>String without address metadata if data adressed to myId, else empty array</returns>
-        private IEnumerable<byte> DeleteAddressMetadata(IEnumerable<byte> data)
-        {
-            var modifiedData = data.ToList();
-            if (!IsPacketToMe(modifiedData))
-            {
-                return new byte[0];
-            }
-
-            modifiedData.RemoveRange(0, 2);
-            return modifiedData;
-        }
-
-        /// <summary>
-        /// Wrap address metadata to data
-        /// </summary>
-        /// <param name="data">Data to wrap</param>
-        /// <returns>Wrapped string</returns>
-        private IEnumerable<byte> WrapAddressMetadata(IEnumerable<byte> data)
-        {
-            var modifiedData = data.ToList();
-
-            modifiedData.Insert(0, MyId);
-            modifiedData.Insert(0, PartnerId);
-
-            return modifiedData;
+            return inputBytes;
         }
 
         /// <summary>
@@ -295,10 +178,7 @@ namespace lab4.backend
         /// <returns>Generated packet</returns>
         private IEnumerable<byte> GeneratePacket(IEnumerable<byte> data)
         {
-            var encodedMeta = Encode(WrapAddressMetadata(data)).ToList();
-            encodedMeta.InsertRange(0, BytesToBools(new[] {StartStopByte}));
-            encodedMeta.AddRange(BytesToBools(new[] {StartStopByte}));
-            return BoolsToBytes(encodedMeta);
+            return data;
         }
 
         /// <summary>
@@ -307,59 +187,11 @@ namespace lab4.backend
         /// <param name="packet">Packet to parse</param>
         /// <param name="index">Index of first unused bool element in packet</param>
         /// <returns>Data from packet if packet addressed to me, else return null</returns>
-        private IEnumerable<byte> ParsePacket(IEnumerable<bool> packet, out int index)
+        private IEnumerable<byte> ParsePacket(IEnumerable<byte> packet, out int index)
         {
-            var listedPackage = packet.ToList();
-            index = 0;
-            bool isStartFound = false;
-            while (index < (listedPackage.Count - BitsInByte + 1))
-            {
-                if (BoolsToBytes(listedPackage.GetRange(index, BitsInByte)).ToArray()[0] == StartStopByte)
-                {
-                    isStartFound = true;
-                    break;
-                }
-                index++;
-            }
-            if (!isStartFound)
-            {
-                throw new CannotFindStartSymbolException();
-            }
-
-            index += BitsInByte;
-            listedPackage.RemoveRange(0, index);
-
-            int decodeIndex;
-
-            var res = DeleteAddressMetadata(BoolsToBytes(Decode(listedPackage, out decodeIndex)));
-            index += decodeIndex;
-            return res;
-        }
-
-        /// <summary>
-        /// Converts byte array to bool (bit) array 
-        /// </summary>
-        /// <param name="data">Data to convert</param>
-        /// <returns>Bit array</returns>
-        private IEnumerable<bool> BytesToBools(IEnumerable<byte> data)
-        {
-            var bitArray = new BitArray(data.ToArray());
-            var bits = new bool[bitArray.Length];
-            bitArray.CopyTo(bits, 0);
-            return bits;
-        }
-
-        /// <summary>
-        /// Converts bool (bit) array to byte array 
-        /// </summary>
-        /// <param name="data">Data to convert</param>
-        /// <returns>Byte array</returns>
-        private IEnumerable<byte> BoolsToBytes(IEnumerable<bool> data)
-        {
-            var bitArray = new BitArray(data.ToArray());
-            var bytes = new byte[(bitArray.Length + BitsInByte - 1) / BitsInByte];
-            bitArray.CopyTo(bytes, 0);
-            return bytes;
+            var enumerable = packet.ToList();
+            index = enumerable.Capacity;
+            return enumerable;
         }
     }
 }
